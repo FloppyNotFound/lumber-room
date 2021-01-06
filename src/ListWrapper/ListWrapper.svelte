35564faa7e3269b9a6bae187806c7fc58ac15044
FloppyNotFound <felix.wagner@outlook.at>
2021-01-06 15:36:04 +0100

    feat: improve error handling, load subfolder items

--- src/ListWrapper/ListWrapper.svelte
+++ src/ListWrapper/ListWrapper.svelte
@@ -1,11 +1,18 @@
 <script lang="ts">
   import { createEventDispatcher } from "svelte";
-  import type { Dropbox, files, Error } from "dropbox";
+  import type {
+    Dropbox,
+    files,
+    Error,
+    DropboxResponseError,
+    auth,
+  } from "dropbox";
 
   export let accessToken: string;
 
   const dispatch = createEventDispatcher();
 
+  let isLoading: boolean;
   let listFolderResult: files.ListFolderResult;
 
   $: hasFolders = !!remoteFolders?.length;
@@ -17,40 +24,68 @@
     listFolderResult?.entries.filter((e) => e[".tag"] === "file")
   );
 
-  const loadItems = (): void => {
+  const loadItems = async (path: string = ""): Promise<void> => {
+    isLoading = true;
+
     // @ts-ignore
     const dbx = new Dropbox.Dropbox({ accessToken });
 
-    (<Dropbox>dbx)
-      .filesListFolder({ path: "", include_media_info: true })
+    await (<Dropbox>dbx)
+      .filesListFolder({ path, include_media_info: true })
       .then((response) => (listFolderResult = response.result))
-      .catch((error: Error<files.ListFolderError>) => {
-        // TODO: improve errorhandling
-        console.error(error);
-        alert(
-          (error.error ?? "<no error>") +
-            " // " +
-            (error.error_summary ?? "<no summary>") +
-            " // " +
-            (error.user_message ?? "<no message>")
-        );
+      .catch(
+        (
+          errorResponse: DropboxResponseError<
+            Error<files.ListFolderError | auth.AuthError>
+          >
+        ) => {
+          if (errorResponse.status === 400) {
+            // TODO: show control instead of alert
+            alert(errorResponse.error);
+            return;
+          }
 
-        // TODO: only dispatch auth error if it really is an auth error
-        // find out how to check if it is an auth error
-        dispatch("autherror");
-      });
+          const tag = errorResponse.error?.error[".tag"];
+
+          if (errorResponse.status === 409 && tag === "path") {
+            // TODO: show control instead of alert
+            alert("Path not found");
+            loadItems("");
+            return;
+          }
+
+          if (
+            [
+              "invalid_access_token",
+              "expired_access_token",
+              "user_suspended",
+            ].includes(tag)
+          ) {
+            dispatch("autherror");
+            return;
+          }
+
+          console.log(tag);
+        }
+      )
+      .finally(() => (isLoading = false));
   };
 </script>
 
 <h2 data-l10n-id="list-wrapper-header">List Wrapper</h2>
 <button
-  on:click={loadItems}
+  on:click={loadItems.bind(this, '')}
+  disabled={isLoading}
   data-l10n-id="list-wrapper-refresh-cta">Refresh</button>
 
 <div>
   {#if hasFolders}
     <p>Folders</p>
-    {#each remoteFolders as folder, i (folder.id)}{i + 1} - {folder.name}{/each}
+    {#each remoteFolders as folder, i (folder.id)}
+      <button on:click={loadItems.bind(this, folder.path_lower)}>{i + 1}
+        -
+        {folder.name}</button>
+    {/each}
   {/if}
 
   {#if hasFiles}
