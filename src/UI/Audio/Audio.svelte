579931dd77787827ad3efc73cc9fc6e69fe25670
Felix Wagner <felix.wagner@outlook.at>
2021-06-19 16:51:22 +0200

    style: #22 ui

--- src/UI/Audio/Audio.svelte
+++ src/UI/Audio/Audio.svelte
@@ -3,30 +3,42 @@
   import audioPlayStore from './audio-play-store';
   import audioRestartStore from './audio-restart-store';
   import type { DownloadAudio } from './download-audio.model';
+  import Separator from '../Separator/Separator.svelte';
 
   export let audio: DownloadAudio;
 
-  $: isPlaying = !isPaused && !hasEnded;
-
   let isPaused = false;
   let hasEnded = false;
-  let currentTime = '0';
-  let duration = '0';
+
+  let currentTimeSeconds = 0;
+  let durationSeconds = 0;
 
   let audioPlayer: HTMLMediaElement;
 
   let updatePlaybackInfoSub: number;
 
+  $: isPlaying = !isPaused && !hasEnded && currentTimeSeconds !== 0;
+  $: currentTime = toPlaybackTime(currentTimeSeconds, false);
+  $: duration = toPlaybackTime(durationSeconds, true);
+
   onMount(() => {
     audioPlayer = document.getElementById('audio-player') as HTMLMediaElement;
 
     updatePlaybackInfoSub = setInterval(() => {
-      duration = toPlaybackTime(audioPlayer.duration);
-      currentTime = toPlaybackTime(audioPlayer.currentTime);
+      currentTimeSeconds = !Number.isNaN(audioPlayer.currentTime)
+        ? Math.round(audioPlayer.currentTime)
+        : 0;
+      durationSeconds = !Number.isNaN(audioPlayer.duration)
+        ? Math.round(audioPlayer.duration)
+        : 0;
+
       isPaused = audioPlayer.paused;
       hasEnded = audioPlayer.ended;
 
-      if (hasEnded) audioPlayStore.set(false);
+      if (hasEnded) {
+        audioPlayStore.set(false);
+        currentTimeSeconds = 0;
+      }
     }, 1000);
   });
 
@@ -63,7 +75,10 @@
     clearInterval(updatePlaybackInfoSub);
   });
 
-  const toPlaybackTime = (timeInSeconds: number): string => {
+  const toPlaybackTime = (
+    timeInSeconds: number,
+    isWithMinutesSuffix: boolean
+  ): string => {
     if (Number.isNaN(timeInSeconds)) return '0';
 
     const minutes = Math.round(timeInSeconds / 60);
@@ -72,32 +87,61 @@
     const seconds = Math.round(timeInSeconds % 60);
     const secondsPrefix = seconds < 10 ? '0' : '';
 
-    return `${minutesPrefix}${minutes}:${secondsPrefix}${seconds} minutes`;
+    const suffix = isWithMinutesSuffix ? ' min' : '';
+
+    return `${minutesPrefix}${minutes}:${secondsPrefix}${seconds}${suffix}`;
   };
 </script>
 
-<div class="audio-wrapper">
-  <div>{audio.name}</div>
-  <div>
-    <span>Is Playing:</span>
-    <span>{isPlaying}</span>
+<div id="audio-wrapper">
+  <div id="audio-wrapper__separator">
+    <Separator text="{audio.name}" />
   </div>
-  <div>
-    <span>Is Paused:</span>
-    <span>{isPaused}</span>
-  </div>
-  <div>
-    <span>Has Ended:</span>
-    <span>{hasEnded}</span>
-  </div>
-  <div>
-    <span>Current time:</span>
-    <span>{currentTime}</span>
-  </div>
-  <div>
-    <span>Duration:</span>
-    <span>{duration}</span>
+  <div id="audio-wrapper__status">
+    <span>Status:</span>
+    <span>
+      {#if isPlaying}
+        Playing
+      {:else}
+        Paused
+      {/if}
+    </span>
   </div>
 
-  <audio id="audio-player" src="{audio.src}" type="audio/mp3"></audio>
+  <div id="audio-wrapper__player">
+    <input
+      id="audio-player-ui"
+      type="range"
+      min="0"
+      max="{durationSeconds}"
+      value="{currentTimeSeconds}"
+      step="1" />
+    <div>
+      {currentTime} / {duration}
+    </div>
+    <audio id="audio-player" src="{audio.src}" type="audio/mp3"></audio>
+  </div>
 </div>
+
+<style lang="scss">
+  #audio-wrapper {
+    &__separator {
+      margin: 0 -0.5rem;
+    }
+
+    &__status {
+      margin-top: 1rem;
+    }
+
+    &__player {
+      margin-top: 0.5rem;
+      margin-right: 0.25rem;
+
+      #audio-player-ui {
+        width: 100%;
+        margin-left: auto;
+        margin-right: auto;
+      }
+    }
+  }
+</style>
